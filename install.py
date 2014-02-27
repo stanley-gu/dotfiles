21f5850a0e4318870a498dccb98e83df304af5ff
Stanley Gu <stanleygu@gmail.com>
2014-02-27 20:29:17 +0000

    Updated with tern and jshint2

--- install.py
+++ install.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import subprocess
 
 if __name__ == "__main__":
     # The first argument of sys.argv is always the filename,
@@ -8,10 +9,20 @@ if __name__ == "__main__":
     dotfiles = ['.vimrc.local', '.vimrc.bundles.local', '.editorconfig']
     for dotfile in dotfiles:
         try:
-            source = os.path.dirname(os.path.realpath(__file__)) + '/' + dotfile
+            source = os.path.dirname(os.path.realpath(__file__)) + \
+                '/' + dotfile
             link = os.environ['HOME'] + '/' + dotfile
             print 'Creating a symlink from ' + link + ' to ' + source
             os.symlink(source, link)
         except OSError as e:
-            print "Problem creating symlink for: {2} \n OSError error({0}): {1}".format(e.errno, e.strerror, dotfile)
+            print("Problem creating symlink for:"
+                  " {2} \n OSError error({0}): {1}").format(
+                e.errno, e.strerror, dotfile)
+            print '\n'
+    subprocess.call(['sudo', 'npm', 'install', '-g', 'jshint',
+                     'grunt-cli', 'bower'])
+    subprocess.call(['vim', '+BundleInstall', '+qall'])
+    subprocess.Popen(['npm', 'install'],
+                     cwd=os.environ['HOME'] +
+                     '/' + '.vim/bundle/tern_for_vim')
     print "Finished running installer"
