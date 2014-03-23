30142c54b4334dd8d6295543ee0cff3f38498d30
Stanley Gu <stanleygu@gmail.com>
2014-03-23 21:25:24 +0000

    Fixed typo and made executable

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "export LC_ALL=en_US.UTF-8" >> ~/.profile
-export "LANG=en_US.UTF-8" >> ~/.profile
+echo "export LANG=en_US.UTF-8" >> ~/.profile
 
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew tap phinze/cask
